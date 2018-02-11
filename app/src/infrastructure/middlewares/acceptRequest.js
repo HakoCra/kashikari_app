b1bb885bdeec855e0f6b40b358fa756d015f9a30
i544c <leo@i544c.me>
2018-02-12 03:59:06 +0900

    Implement acceptRequest

--- app/src/infrastructure/middlewares/acceptRequest.js
+++ app/src/infrastructure/middlewares/acceptRequest.js
@@ -2,11 +2,12 @@ import boundActionCreator from '../../ui/boundActionCreator';
 import * as app_types from '../../application/types';
 import * as infra_types from '../../infrastructure/types';
 
-const processAcceptRequest = (user, state) => {
+const processAcceptRequest = (id, state) => {
   const token = state.application.user.token;
   const api_server = state.infrastructure.api_server;
-  fetch(`${api_server}/requests`, {
-    method: 'GET',
+  console.log(`${api_server}/request/${id}/accept`)
+  fetch(`${api_server}/request/${id}/accept`, {
+    method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
@@ -16,18 +17,7 @@ const processAcceptRequest = (user, state) => {
     .then(res => res.json())
     .then(res => {
       if(res.error !== 'Authorization error'){
-        const requests = res.map((val) => {
-          return {
-            id: val.id,
-            title: val.title,
-            timelimit: val.timelimit,
-            reward: val.reward,
-            comment: val.comment
-          }
-        })
-        boundActionCreator(app_types.SET_REQUESTS, {
-          requests: requests
-        });
+        console.log(res)
       }else{
         boundActionCreator(infra_types.SET_LOGIN_AVAILABLE, {available: false});
         boundActionCreator(infra_types.SET_LOGIN_FAILED, {failed: true});
@@ -38,8 +28,7 @@ const processAcceptRequest = (user, state) => {
 const acceptRequest = store => next => action => {
   if(action.type === app_types.ACCEPT_REQUESTS)
   {
-    // todo: implement
-    console.log('accept', action.id)
+    processAcceptRequest(action.id, store.getState());
   }
   next(action);
 };
